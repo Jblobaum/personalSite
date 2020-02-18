4f4af882263df42be1a4d58906d87309914f7a2c
Justin Blobaum <justinblobaum@Justins-MacBook-Air.local>
2020-02-18 09:00:45 -0600

    created project added header, side nav, and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule} from '@angular/forms'
+import { MaterialModule } from './materialModule';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
