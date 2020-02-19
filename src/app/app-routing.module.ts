eef17715a2dfd7b4a0a90e1fd7f3e600cf5726a3
Justin Blobaum <justinblobaum@Justins-MacBook-Air.local>
2020-02-18 23:42:15 -0600

    added portfolio and games component, added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { GamesComponent } from './games/games.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '**', component: HomeComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'games', component: GamesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule} from '@angular/forms'
 import { MaterialModule } from './materialModule';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { GamesComponent } from './games/games.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PortfolioComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
